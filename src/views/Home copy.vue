2eed2f894b6badb70f8b94f75f90dd8d928f72ae
Marco Moosler <moosler@gmx.de>
2020-01-22 21:20:21 +0100

    chart added

--- src/views/Home copy.vue
+++ src/views/Home copy.vue
@@ -16,26 +16,48 @@
           ></v-file-input>
         </v-card>
       </v-col>
-    </v-row>
-    <!-- chart  -->
-    <v-row class="grey lighten-5" align="center" justify="center">
       <v-col cols="auto">
-        <v-card min-width="700" color="light-green lighten-4" outlined>
+        <v-card outlined>
           <v-card-title>
-            <span class="title font-weight-bold">Chart</span>
+            <span class="title font-weight-light">ParamsBlock</span>
+            <v-text-field v-model="search" label="Filter"></v-text-field>
           </v-card-title>
-          <div id="main" style="width:700px; height:400px;">
-            <v-chart :options="chartOptions" />
-          </div>
+          <v-data-table :headers="fileHeaders" :items="fileblocks" :search="search"></v-data-table>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!-- Trace Data -->
+    <v-row class="grey lighten-1" align="center" justify="center">
+      <v-col cols="auto">
+        <v-card class="mx-auto text-center" color="green" dark max-width="600">
+          <v-card-text>
+            <v-sheet color="rgba(0, 0, 0, .12)">
+              <v-sparkline
+                :value="chartValues"
+                color="rgba(255, 255, 255, .7)"
+                height="100"
+                padding="24"
+                stroke-linecap="round"
+                smooth
+              >
+                <template v-slot:label="item">{{ item.value }}</template>
+              </v-sparkline>
+            </v-sheet>
+          </v-card-text>
+
+          <v-card-text>
+            <div class="display-1 font-weight-thin">Trace File</div>
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
     <!-- Single Table Elements -->
     <v-row class="grey lighten-5" align="center" justify="center">
       <v-col cols="auto">
+        <h3>Resumé</h3>
         <v-card outlined>
           <v-card-title>
-            <span class="title font-weight-bold">Summary</span>
+            <span class="title font-weight-light">Summary</span>
           </v-card-title>
           <v-data-table
             :headers="extractHeaders(dumpdata.KeyEvents['Summary'])"
@@ -45,7 +67,7 @@
         </v-card>
         <v-card outlined>
           <v-card-title>
-            <span class="title font-weight-bold">Events</span>
+            <span class="title font-weight-light">Events</span>
           </v-card-title>
           <v-data-table
             :headers="extractHeaders(dumpdata.KeyEvents['event 1'])"
@@ -55,12 +77,24 @@
         </v-card>
       </v-col>
     </v-row>
-
+    <!-- chart  -->
+    <v-row class="grey lighten-5" align="center" justify="center">
+      <v-col cols="auto">
+        <v-card min-width="700" color="light-blue lighten-4" outlined>
+          <v-card-title>
+            <span class="title font-weight-light">Chart</span>
+          </v-card-title>
+          <div id="main" style="width:700px; height:400px;">
+            <v-chart :options="chartOptions" />
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
     <!-- Data in List form -->
     <v-container fluid>
       <v-row class="grey lighten-3" align="center" justify="center">
         <v-col v-for="(value, key) in filterDumpfile" v-bind:key="value.id" cols="auto">
-          <v-card class="mx-auto" max-width="400" tile>
+          <v-card class="mx-auto" max-width="300" tile>
             <v-list dense>
               <v-subheader>{{key}}</v-subheader>
               <v-list-item v-for="(item, key2) in value" :key="key2">
@@ -91,10 +125,22 @@
         </v-col>
       </v-row>
     </v-container>
+    <!-- All Data as a new Table -->
+    <!-- <v-row class="grey lighten-1" align="center" justify="center">
+      <v-col v-for="(value, key) in dumpdata" v-bind:key="value.id" cols="auto">
+        <v-card outlined>
+          <v-card-title>
+            <span class="title font-weight-light">{{key}}</span>
+          </v-card-title>
+          <v-data-table :headers="extractHeaders(value)" :items="[value]" hide-default-footer></v-data-table>
+        </v-card>
+      </v-col>
+    </v-row>-->
   </v-container>
 </template>
 
 <script>
+import filedata from "../assets/data/sample.json";
 import dump from "../assets/data/dump.json";
 import ECharts from "vue-echarts";
 import * as trace from "../assets/js/traceValues.js";
@@ -111,6 +157,25 @@ export default {
   computed: {
     dumpdata() {
       return dump;
+    },
+    fileHeaders() {
+      let headerData = [];
+      for (let block in filedata.blocks) {
+        for (let item in filedata.blocks[block]) {
+          let obj = { text: item, value: item };
+          headerData.push(obj);
+        }
+        break;
+      }
+      return headerData;
+    },
+    fileblocks() {
+      let blockData = [];
+      for (let block in filedata.blocks) {
+        let obj = filedata.blocks[block];
+        blockData.push(obj);
+      }
+      return blockData;
     },
     filterKeyEvent() {
       let eventfile = [];
@@ -145,6 +210,7 @@ export default {
   },
   data() {
     return {
+      test: trace.yVal,
       isObject(obj) {
         if (typeof obj === "object" && obj !== null) {
           return true;
@@ -161,6 +227,7 @@ export default {
       },
       items: ["JDSU MTS600", "EXfo"],
       search: "",
+      chartValues: [423, 446, 675, 510, 590, 610, 760],
       chartOptions: {
         animation: false,
         title: {
